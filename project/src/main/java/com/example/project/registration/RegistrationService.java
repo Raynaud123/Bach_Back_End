b4760cd3af1646b42ce2d89f8086cf02206748b2
Marieke Beke <marieke.beke@student.kuleuven.be>
2022-02-24 00:01:05 +0100

    Login commit

--- project/src/main/java/com/example/project/registration/RegistrationService.java
+++ project/src/main/java/com/example/project/registration/RegistrationService.java
@@ -1,10 +1,31 @@
 package com.example.project.registration;
 
+import com.example.project.appuser.AppUser;
+import com.example.project.appuser.AppUserRole;
+import com.example.project.appuser.AppUserService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class RegistrationService {
+
+    private final AppUserService appUserService;
+    private final EmailValidator emailValidator;
+
     public String register(RegistrationRequest request) {
-        return "works";
+        boolean isValidEmail = emailValidator.test(request.getEmail());
+        if (!isValidEmail) {
+            throw new IllegalStateException("email not valid");
+        }
+        return appUserService.singUpUser(
+                new AppUser(
+                        request.getFirstName(),
+                        request.getLastName(),
+                        request.getEmail(),
+                        request.getPassword(),
+                        AppUserRole.USER
+                )
+        );
     }
 }
