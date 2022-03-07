c6362e09bf422a8e58168141364fec3912079a6e
raynaud <raynaud.cornille@gmail.com>
2022-03-07 17:27:40 +0100

    promotor class

---
+++ project/src/main/java/com/example/project/promotor/Promotor.java
@@ -0,0 +1,13 @@
+package com.example.project.promotor;
+
+import com.example.project.person.Person;
+import com.example.project.student.Student;
+import com.example.project.topic.Topic;
+
+public class Promotor extends Person {
+
+    Topic topic;
+    Student boostedStudent;
+
+
+}
