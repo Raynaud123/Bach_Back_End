d1ae3d55b7ad9b0a2d5ad7a08c8f1f1bfdd98cae
Marthe Spriet <marthe.spriet2001@gmail.com>
2022-03-08 11:04:15 +0100

    Aanpassingen Student - Opvragen approved topics

--- project/src/main/java/com/example/project/student/StudentController.java
+++ project/src/main/java/com/example/project/student/StudentController.java
@@ -1,9 +1,13 @@
 package com.example.project.student;
 
+import com.example.project.topic.Topic;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @ResponseBody
@@ -13,5 +17,6 @@ public class StudentController {
     @Autowired
     private StudentService service;
 
-
+    @GetMapping(path = "/topics/approved")
+    public List<Topic> getAllApprovedTopics() { return service.findAllApprovedTopics(); }
 }
--- project/src/main/java/com/example/project/student/StudentService.java
+++ project/src/main/java/com/example/project/student/StudentService.java
@@ -1,8 +1,30 @@
 package com.example.project.student;
 
+import com.example.project.topic.Topic;
+import com.example.project.topic.TopicRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class StudentService {
+
+    @Autowired
+    private TopicRepository topicRepository;
+
+    public List<Topic> findAllApprovedTopics() {
+
+        List<Topic> appr = new ArrayList<Topic>();
+        List<Topic> all = topicRepository.findAll();
+
+        for(Topic top: all){
+            if(top.isApproved()){
+                appr.add(top);
+            }
+        }
+        return appr;
+    }
 
 }
