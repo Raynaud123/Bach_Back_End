f07d13a6f6961f9622e8b68c4c2dd697753baf88
Marieke Beke <marieke.beke@student.kuleuven.be>
2022-03-15 14:02:47 +0100

    Topic approve mogelijk & h2 is terug pgadmin

--- project/src/main/java/com/example/project/topic/TopicController.java
+++ project/src/main/java/com/example/project/topic/TopicController.java
@@ -1,20 +1,31 @@
 package com.example.project.topic;
 
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @ResponseBody
-@RequestMapping
+@RequestMapping(path = "topics")
+@AllArgsConstructor
 public class TopicController {
+
+    //private final TopicService topicService;
 
     @Autowired
     private TopicService topicService;
 
+    @PutMapping
+    public String updateApprove(@RequestBody ApproveRequest request) {
+        return topicService.updateApprove(request);
+    }
+
     @CrossOrigin(origins = "http://localhost:3000")
-    @GetMapping(path = "/topics/approved")
+    @GetMapping(path = "/approved")
     public List<Topic> getAllApprovedTopics() { return topicService.findAllApprovedTopics(); }
 
     /* werkt nog niet -> geblokt door CORS
@@ -22,4 +33,7 @@ public class TopicController {
     @PostMapping
     public void registerNewTopic(@RequestBody Topic topic) { topicService.addNewTopic(topic); }
     */
+
+
+
 }
--- project/src/main/java/com/example/project/topic/TopicService.java
+++ project/src/main/java/com/example/project/topic/TopicService.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class TopicService {
@@ -27,4 +28,16 @@ public class TopicService {
         }
         return appr;
     }
+
+    public String updateApprove(ApproveRequest request) {
+        Long id = request.getTopic_id();
+        if (topicRepository.findById(id).isPresent()) {
+            Topic testObject = topicRepository.findById(id).get();
+            testObject.setApproved_topic(request.getApproved());
+            topicRepository.save(testObject);
+            return "Topic approved status is updated";
+        }
+        else return "Topic not found";
+    }
+
 }
