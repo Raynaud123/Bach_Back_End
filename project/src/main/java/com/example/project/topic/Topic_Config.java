b949acfbce623fc36560cba6807c93099da416e1
Marthe Spriet <marthe.spriet2001@gmail.com>
2022-03-29 09:41:56 +0200

    Topics bij -> niet voor demo uitvoeren

--- project/src/main/java/com/example/project/topic/Topic_Config.java
+++ project/src/main/java/com/example/project/topic/Topic_Config.java
@@ -4,35 +4,34 @@
 //import org.springframework.context.annotation.Bean;
 //import org.springframework.context.annotation.Configuration;
 //
-//import java.text.ParseException;
 //import java.text.SimpleDateFormat;
 //import java.util.List;
 //
 //@Configuration
 //public class Topic_Config {
 //
-//    String dateString1 = "10/03/2022";
+//    String dateString1 = "29/03/2022";
 //    SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
 //
-//    public Topic_Config() throws ParseException {
+//    public Topic_Config() {
 //    }
 //    @Bean
 //    CommandLineRunner commandLineRunner(TopicRepository repository) {
 //        return args -> {
 //            Topic topic1 = new Topic(
-//                    1L,"Topic1", 1L, null, null, null, null, null, true, false, "Description1", format.parse(dateString1)
+//                    6L,"Topic6", 5L,null,  null, null, null, null, null, true, false, "Description6", format.parse(dateString1)
 //            );
 //            Topic topic2 = new Topic(
-//                    2L,"Topic2", 2L, null, null, null, null, null, false, false, "Description2", format.parse(dateString1)
+//                    7L,"Topic7", 5L, null,null, null, null, null, null, false, false, "Description7", format.parse(dateString1)
 //            );
 //            Topic topic3 = new Topic(
-//                    3L,"Topic3", 3L, null, null, null, null, null, true, true, "Description3", format.parse(dateString1)
+//                    8L,"Topic8", null, null, null, null, null, null, null, true, true, "Description8", format.parse(dateString1)
 //            );
 //            Topic topic4 = new Topic(
-//                    4L,"Topic4", 3L, null, null, null, null, null, true, true, "Description4", format.parse(dateString1)
+//                    9L,"Topic9", null, null, null, null, null, null, null, true, true, "Description9", format.parse(dateString1)
 //            );
 //            Topic topic5 = new Topic(
-//                    5L,"Topic5", 3L, null, null, null, null, null, true, true, "Description5", format.parse(dateString1)
+//                    10L,"Topic10", 5L, null, null, null, null, null, null, true, true, "Description10", format.parse(dateString1)
 //            );
 //            repository.saveAll(
 //                    List.of(topic1,topic2,topic3,topic4,topic5)
