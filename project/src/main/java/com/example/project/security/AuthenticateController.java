86aa6425b1218ad441f3357d3cadfa56c40dbcc8
raynaud <raynaud.cornille@gmail.com>
2022-04-04 09:14:02 +0200

    Security eraf

---
+++ project/src/main/java/com/example/project/security/AuthenticateController.java
@@ -0,0 +1,12 @@
+package com.example.project.security;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class AuthenticateController {
+
+
+
+}
