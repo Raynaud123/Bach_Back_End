6c7fa6b72a0be83ab3d398526351fe02be691032
Marieke Beke <marieke.beke@student.kuleuven.be>
2022-04-12 15:59:24 +0200

    Masterproefcoordinator attributen

--- project/src/main/java/com/example/project/Coordinator/Coordinator.java
+++ project/src/main/java/com/example/project/Coordinator/Coordinator.java
@@ -1,19 +1,41 @@
 package com.example.project.Coordinator;
 
+import com.example.project.keyword.Keyword;
 import com.example.project.person.Person;
+import com.example.project.promotor.Promotor;
+import com.example.project.topic.Topic;
+import com.example.project.topicprovider.TopicProvider;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import javax.persistence.DiscriminatorValue;
-import javax.persistence.Entity;
+
+import javax.persistence.*;
 import java.io.Serializable;
+import java.util.List;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @Entity
-@DiscriminatorValue("coordinator")
+@DiscriminatorValue("master")
 public class Coordinator extends Person implements Serializable {
 
+    @SequenceGenerator(
+            name = "master_sequence",
+            sequenceName = "master_sequence",
+            allocationSize = 1
+    )
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "master_sequence"
+    )
+    private Long master_id;
 
+    @ManyToMany
+    private List<Topic> topic_list;
+    @ManyToMany
+    private List<TopicProvider> topicProvider_list;
+    @ManyToMany
+    private List<Promotor> promotor_list;
 }
