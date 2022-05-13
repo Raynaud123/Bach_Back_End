bde53259e10315a43e1f306a4a6d57ff6a209aad
raynaud <raynaud.cornille@gmail.com>
2022-05-13 11:44:26 +0200

    checken op targetaudiene en top 3 student

--- project/src/main/java/com/example/project/student/StudentController.java
+++ project/src/main/java/com/example/project/student/StudentController.java
@@ -2,18 +2,25 @@ package com.example.project.student;
 
 import com.example.project.exceptions.IdNotFoundRequestException;
 import com.example.project.exceptions.NietApprovedRequestException;
+import com.example.project.topic.BoostStudentRequest;
 import com.example.project.topic.Topic;
 import com.example.project.topic.TopicService;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 import java.util.Optional;
 
 @RestController
 @ResponseBody
-@CrossOrigin(origins = "http://localhost:3000")
+//@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping(path = "student")
+@AllArgsConstructor
 public class StudentController {
 
     @Autowired
@@ -48,9 +55,9 @@ public class StudentController {
         return studentService.findAllNotHidedStudents();
     }
 
-    @GetMapping(path = "/hided/{id}")
-    public List<Student> getAllNotHidedMasterProefIdStudents(@PathVariable long id) throws IdNotFoundRequestException {
-        return studentService.findAllNotHidedMasterproefIDStudents(id);
+    @GetMapping(path = "/hided/{id}/{topicid}")
+    public List<Student> getAllNotHidedMasterProefIdStudents(@PathVariable long id, @PathVariable long topicid) throws IdNotFoundRequestException {
+     return studentService.findAllNotHidedMasterproefIDStudents(id,topicid);
     }
 
     @GetMapping(path = "/{id}/preferred/all")
